e3fe4a6152ad9d72391d2bb730bd4d3b32d6d10f
TopGitSan <c.toparcean@gmail.com>
2019-04-21 18:13:09 +0200

    add functionality to login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getUserDetails(){
+  getUserDetails(username,password){
     // post these details to API server and return user info if correct
+    // enter the url of the server
+    // the server checks users input 
+    return this.http.post('[url]',{
+      username,
+      password
+    })
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../auth.service";
+
 
 @Component({
   selector: 'app-login',
@@ -7,14 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Auth: AuthService) { }
 
   ngOnInit() {
   }
 
-  loginUser(evet){
+  loginUser(evet) {
     event.preventDefault();
-    console.log(event)
+
+    const target = event.target as HTMLElement;
+    const username = (target.querySelector('#user') as HTMLInputElement).value;
+    const password = (target.querySelector('#user') as HTMLInputElement).value;
+
+    this.Auth.getUserDetails(username,password).subscribe((data:any) =>{
+      if(data.success) {
+        // redirect the user to /admin
+      } else {
+        window.alert("Invalid credentials");
+      }
+    })
+    console.log(username, password);
   }
 
 
