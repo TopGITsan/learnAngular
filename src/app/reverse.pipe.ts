d84d0b9170ae299d98afd913b5c0e4df74b697d9
TopGitSan <c.toparcean@gmail.com>
2019-05-05 18:27:38 +0200

    add functionality to the custom reverse pipe

--- src/app/reverse.pipe.ts
+++ src/app/reverse.pipe.ts
@@ -5,8 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReversePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any, ...args): any {
+    console.log(value, args)
+    let newString = value;
+    if (args[0]){ // i want a full stop "."
+      newString += "."
+    }
+    if (args[1] === "dot"){
+      newString= " BOT " + newString;
+    }
+    return newString;
   }
 
 }
