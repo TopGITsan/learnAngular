91e2347e138c0a4716a2d4ab06d6aee1c7aed1d1
TopGitSan <c.toparcean@gmail.com>
2019-05-05 19:26:47 +0200

    use a custom directive to render

--- src/app/red-black.directive.ts
+++ src/app/red-black.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
+import { element } from '@angular/core/src/render3';
 
 @Directive({
   selector: '[appRedBlack]'
 })
 export class RedBlackDirective {
 
-  constructor() { }
+  element: ElementRef;
+
+  constructor(private elem: ElementRef) {
+    console.log(elem);
+    elem.nativeElement.style.color = "red";
+    elem.nativeElement.style.backgroundColor = "black";
+    this.element = elem;
+  }
+
+  ngOnInit(){
+    this.element.nativeElement.innerText += " - rendered by custom appRedblack directive"
+  }
 
 }
